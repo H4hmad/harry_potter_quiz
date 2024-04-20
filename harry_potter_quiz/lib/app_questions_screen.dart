bbd016310b8a76388522e7c49a759cc1edf4222d
Learning-Pythong <lamostlaaa@gmail.com>
2024-04-20 20:55:51 +1000

    Organizing Questions Screen

--- harry_potter_quiz/lib/app_questions_screen.dart
+++ harry_potter_quiz/lib/app_questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:harry_potter_quiz/app_answer_button.dart';
+import 'package:harry_potter_quiz/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,19 +11,32 @@ class QuestionsScreen extends StatefulWidget {
 
 //a
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestion = questions[0];
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Questions Screen'),
-          const SizedBox(height: 40),
-          AnswerButton(answerText: " ANSWER 1", onAnswerSelect: () {}),
-          AnswerButton(answerText: " ANSWER 2", onAnswerSelect: () {}),
-          AnswerButton(answerText: " ANSWER 3", onAnswerSelect: () {}),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                currentQuestion.questionText,
+                style: const TextStyle(color: Colors.black),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 40),
+            ...currentQuestion.answerList.map((answer) {
+              return AnswerButton(answerText: answer, onAnswerSelect: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
